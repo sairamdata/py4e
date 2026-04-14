03162daf55cf54147c9f86e1e785931961e4c9e2
sairamdata <67458187+sairamdata@users.noreply.github.com>
2026-04-14 04:49:23 +0000

    "I have impletment a simple smart calculator"

---
+++ smart_calculator.py
@@ -0,0 +1,32 @@
+import sys
+
+operators_supported = ['+','-','*','/']
+
+print("Input")
+
+try:
+    first_num = float(input("Enter first number: "))
+    get_operator = input("Enter operator (+ - * /): ")
+    second_num = float(input("Enter second number: "))
+except:
+    print("Enter valid numerical values!!!")
+    sys.exit()    
+
+result = None
+if get_operator in operators_supported:
+    if get_operator == '+':
+        result = first_num + second_num
+    elif get_operator == '-':
+        result = first_num - second_num
+    elif get_operator == '*':
+        result = first_num * second_num
+    elif get_operator == '/':
+        result = first_num / second_num
+    
+    if result - int(result) == 0:
+        result = int(result)
+    
+    print('Output:')
+    print("Result:", result)
+else:
+    print("Enter valid operators")
